e93c4b7a0852e6b6355b787a7408b1d1f1ddde22
ghost <kirk.douglas.richardson@gmail.com>
2017-09-12 12:56:01 -0600

    html rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+const marked = require('marked');
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      mkd: ''
+    }
+  }
+
+  handleChange = (event) => {
+    const input = event.target.value;
+    const mkdHtml = marked(input);
+    // const parser = new DOMParser();
+    // const test = parser.parseFromString(testStr, "text/html");
+    this.setState({
+      input: input,
+      mkd: mkdHtml
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        
+
+              <textarea
+                className="text"
+                placeholder = {"Enter markdown here"}
+                wrap="soft"
+                value={ this.state.input }
+                onChange={this.handleChange}
+                />
+
+              <textarea
+                className="text"
+                placeholder = {"Preview will appear here"}
+                value = {this.state.mkd}
+                disabled
+                />
+
+
       </div>
     );
   }
